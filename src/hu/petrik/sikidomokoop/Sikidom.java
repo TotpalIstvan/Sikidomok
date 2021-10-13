f9a5874639123c1bfdccdf2efac0cd7953398098
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-13 16:07:20 +0200

    sikidomok 1.feladat kész

--- src/hu/petrik/sikidomokoop/Sikidom.java
+++ src/hu/petrik/sikidomokoop/Sikidom.java
@@ -1,4 +1,19 @@
 package hu.petrik.sikidomokoop;
 
-public class Sikidom {
+import java.util.Random;
+
+public abstract class Sikidom {
+    private final static Random random = new Random();
+
+    protected final double veletlenHossz() {
+        return random.nextDouble() * 10 + 5;
+    }
+    protected abstract double keruletSzamit();
+    protected abstract double teruletSzamit();
+
+    @Override
+    public String toString() {
+        return String.format("K = %f;T = %f", keruletSzamit(), teruletSzamit());
+
+    }
 }
